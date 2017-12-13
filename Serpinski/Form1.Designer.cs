6120cde4f854cfd133f9d497fc88ee69189259e0
Trefilov Ilya <fallensoul24@gmail.com>
2017-12-13 23:59:01 +0500

    Добавьте файлы проекта.

---
+++ Serpinski/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Serpinski
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CarpetButton = new System.Windows.Forms.Button();
+            this.FractalPictureBox = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.FractalPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CarpetButton
+            // 
+            this.CarpetButton.Location = new System.Drawing.Point(22, 11);
+            this.CarpetButton.Name = "CarpetButton";
+            this.CarpetButton.Size = new System.Drawing.Size(125, 32);
+            this.CarpetButton.TabIndex = 1;
+            this.CarpetButton.Text = "Построить";
+            this.CarpetButton.UseVisualStyleBackColor = true;
+            this.CarpetButton.Click += new System.EventHandler(this.CarpetButton_Click);
+            // 
+            // FractalPictureBox
+            // 
+            this.FractalPictureBox.Location = new System.Drawing.Point(22, 49);
+            this.FractalPictureBox.Name = "FractalPictureBox";
+            this.FractalPictureBox.Size = new System.Drawing.Size(550, 550);
+            this.FractalPictureBox.TabIndex = 2;
+            this.FractalPictureBox.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(153, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(154, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(584, 611);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.FractalPictureBox);
+            this.Controls.Add(this.CarpetButton);
+            this.Name = "Form1";
+            this.Text = "Serpinski";
+            ((System.ComponentModel.ISupportInitialize)(this.FractalPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button CarpetButton;
+        private System.Windows.Forms.PictureBox FractalPictureBox;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Serpinski/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Serpinski
+{
+    public partial class Form1 : Form
+    {
+        private int width;
+        private int height;
+        private Bitmap fractal;
+        private Graphics graph;
+
+        public Form1()
+        {
+            InitializeComponent();
+            width = FractalPictureBox.Width;
+            height = FractalPictureBox.Height;
+        }
+
+
+        private void CarpetButton_Click(object sender, EventArgs e)
+        {
+            int Level;
+            if (textBox1.Text == "") Level = 0;
+            else
+            Level = Convert.ToInt32(textBox1.Text);
+            
+            fractal = new Bitmap(width, height);
+            graph = Graphics.FromImage(fractal);
+            RectangleF carpet = new RectangleF(0, 0, width, height);
+            DrawCarpet(Level, carpet);
+            FractalPictureBox.BackgroundImage = fractal;
+        }
+
+        public PointF MidPoint(PointF p1, PointF p2)
+        {
+            return new PointF((p1.X + p2.X) / 2f, (p1.Y + p2.Y) / 2f);
+        }
+
+        public void DrawCarpet(int level, RectangleF carpet)
+        {
+            if (level == 0)
+            {
+                graph.FillRectangle(Brushes.Black, carpet);
+            }
+            else
+            {
+
+                var width = carpet.Width / 3;
+                var height = carpet.Height / 3;
+                var x1 = carpet.Left;
+                var x2 = x1 + width;
+                var x3 = x1 + 2 * width;
+
+                var y1 = carpet.Top;
+                var y2 = y1 + height;
+                var y3 = y1 + 2 * height;
+
+                DrawCarpet(level - 1, new RectangleF(x1, y1, width, height)); // левый 1(верхний)
+                DrawCarpet(level - 1, new RectangleF(x2, y1, width, height)); // средний 1
+                DrawCarpet(level - 1, new RectangleF(x3, y1, width, height)); // правый 1
+                DrawCarpet(level - 1, new RectangleF(x1, y2, width, height)); // левый 2
+                DrawCarpet(level - 1, new RectangleF(x3, y2, width, height)); // правый 2
+                DrawCarpet(level - 1, new RectangleF(x1, y3, width, height)); // левый 3
+                DrawCarpet(level - 1, new RectangleF(x2, y3, width, height)); // средний 3
+                DrawCarpet(level - 1, new RectangleF(x3, y3, width, height)); // правый 3
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
